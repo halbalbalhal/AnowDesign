5f21fb848deaf2afd898d6eda316579d7768218a
halbalbalhal <aleksandrbirula3@gmail.com>
2023-02-14 16:03:11 +0300

    14.02.2023 update slider

--- components/Swiper/swiper.js
+++ components/Swiper/swiper.js
@@ -1,7 +1,9 @@
 class Swiper {
     render() {
         const html = `
-            <span class="title">Meet Our Awesome Team</span>
+               <div class="swiper__title title">
+                   <span class="title">Meet Our Awesome Team</span>
+               </div>
             <swiper-container class="swiper__wrapper">
               <swiper-slide class="swiper__block">
                         <div class="team__members-block">
@@ -10,26 +12,40 @@ class Swiper {
                         </div>                                                             
               </swiper-slide>
               
-              <swiper-slide>
-                        <div class="team__members-block">
+              <swiper-slide class="swiper__block">
+                        <div class="team__members-block member__andy">
                             <span class="team__members-title">Anna Brown</span>
                             <span class="team__members-text">Creative Director</span>
                         </div>               
               </swiper-slide>
               
-              <swiper-slide>
-                        <div class="team__members-block">
+              <swiper-slide class="swiper__block">
+                        <div class="team__members-block member__katy">
                             <span class="team__members-title">Anna Brown</span>
                             <span class="team__members-text">Creative Director</span>
                         </div>               
               </swiper-slide>
               
-              <swiper-slide>
-                        <div class="team__members-block">
+              <swiper-slide class="swiper__block">
+                        <div class="team__members-block member__margo">
                             <span class="team__members-title">Anna Brown</span>
                             <span class="team__members-text">Creative Director</span>
                         </div>               
               </swiper-slide>
+              
+              <swiper-slide class="swiper__block">
+                        <div class="team__members-block member__kanye">
+                            <span class="team__members-title">Anna Brown</span>
+                            <span class="team__members-text">Creative Director</span>
+                        </div>               
+              </swiper-slide>
+              
+              <swiper-slide class="swiper__block">
+                        <div class="team__members-block member__marta">
+                            <span class="team__members-title">Anna Brown</span>
+                            <span class="team__members-text">Creative Director</span>
+                        </div>               
+              </swiper-slide>              
             </swiper-container>
         `
         ROOT_SWIPER.innerHTML = html
