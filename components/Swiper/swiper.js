c37f44638ef0f2fa91079ff9a2832c86391fb491
halbalbalhal <aleksandrbirula3@gmail.com>
2023-02-14 15:38:17 +0300

    fix 14.02.2023

--- components/Swiper/swiper.js
+++ components/Swiper/swiper.js
@@ -1,21 +1,38 @@
 class Swiper {
     render() {
         const html = `
-            <swiper-container>
-              <swiper-slide class="swiper-block">
+            <span class="title">Meet Our Awesome Team</span>
+            <swiper-container class="swiper__wrapper">
+              <swiper-slide class="swiper__block">
                         <div class="team__members-block">
                             <span class="team__members-title">Anna Brown</span>
                             <span class="team__members-text">Creative Director</span>
                         </div>                                                             
               </swiper-slide>
-              <swiper-slide>Slide 2</swiper-slide>
-              <swiper-slide>Slide 3</swiper-slide>
-              <swiper-slide>Slide ...</swiper-slide>
+              
+              <swiper-slide>
+                        <div class="team__members-block">
+                            <span class="team__members-title">Anna Brown</span>
+                            <span class="team__members-text">Creative Director</span>
+                        </div>               
+              </swiper-slide>
+              
+              <swiper-slide>
+                        <div class="team__members-block">
+                            <span class="team__members-title">Anna Brown</span>
+                            <span class="team__members-text">Creative Director</span>
+                        </div>               
+              </swiper-slide>
+              
+              <swiper-slide>
+                        <div class="team__members-block">
+                            <span class="team__members-title">Anna Brown</span>
+                            <span class="team__members-text">Creative Director</span>
+                        </div>               
+              </swiper-slide>
             </swiper-container>
         `
         ROOT_SWIPER.innerHTML = html
-
-        
     }
 
 }
